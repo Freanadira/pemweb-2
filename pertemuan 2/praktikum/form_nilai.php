562ae0aa7b3c6e76bec5da64a3700e497e0e62b3
Freanadira <freanadirasolihah@gmail.com>
2024-02-28 15:29:00 +0700

    praktikum 2

--- pertemuan 2/praktikum/form_nilai.php
+++ pertemuan 2/praktikum/form_nilai.php
@@ -10,10 +10,10 @@
 </head>
 
 <body>
-    <h2>Form Nilai Siswa</h2>
+    <h2 class="text-center">Form Nilai Siswa</h2>
     <hr>
 
-    <form class="mr-5 ml-5">
+    <form class="mr-5 ml-5" action="proses_nilai.php" method="get">
         <div class="form-group row">
             <label for="nama" class="col-4 col-form-label">Nama</label>
             <div class="col-8">
