5ca6f412d4f068497416312296677afa0d4726e0
Freanadira <freanadirasolihah@gmail.com>
2024-02-28 12:17:18 +0700

    coba

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+
+// array indexing
+$mhs1 = ['011', 'Frea', 'Sistem Informasi', [3.5, 3.7, 4.0]];
+
+// panggil array
+echo 'Nama: ' . $mhs1[1] . '<br>'; // konkatenasi string
+echo "NIM: $mhs1[1] <br>";
+echo 'IPS semester 1: ' . $mhs1[3] [0]; // memanggil array dalam array
